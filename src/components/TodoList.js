2adb28d0acae0566c256d849a684814b77be164f
Hyehyun1001 <hye22327@naver.com>
2024-01-15 12:50:25 +0900

    혜현수정

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+<<<<<<< Updated upstream
 import TodoItem from './TodoItem';
 import "./TodoList.css";
 
@@ -7,34 +8,64 @@ const TodoList = ({ todo , onUpdate, onDelete }) => {
     const onChangeSearch = (e) => {
         setSearch(e.target.value);
     };
+=======
+import "./TodoList.css";
+
+
+const TodoList = ({ todo, onUpdate, onDelete }) => {
+    const [search, setSearch] = useState("");
+
+    const onChangeSearch = (e) => {
+        setSearch(e.target.value);
+    };
+
+>>>>>>> Stashed changes
+    const handleSearch = () => {
+        const searchResult = getSearchResult();
+        console.log("검색 결과:", searchResult);
+    };
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
     const getSearchResult = () => {
         return search === ""
             ? todo
-            : todo.filter((it) => 
+            : todo.filter((it) =>
+<<<<<<< Updated upstream
                 it.content && it.content.toLowerCase().includes(search.toLowerCase())
+=======
+                it.content.toLowerCase().includes(search.toLowerCase())
+>>>>>>> Stashed changes
             );
     };
-    
-    return (
-    <div className="TodoList">
-        <input 
-            value={search}
-            onChange={onChangeSearch}
-            className="searchbar"
-            placeholder="검색어를 입력하세요" 
-        />
 
-        <div className='list_wrapper'>
-            {getSearchResult().map((it) => (
-                <TodoItem 
-                key = {it.id} 
-                {...it} 
-                onUpdate = {onUpdate}
-                onDelete = {onDelete}
-                />
-            ))}
+    return (
+        <div className="TodoList">
+            <input
+                value={search}
+                onChange={onChangeSearch}
+                className="searchbar"
+                placeholder="검색어를 입력하세요"
+            />
+            <button onClick={handleSearch} className="searchButton">
+                🔍
+            </button>
+<<<<<<< Updated upstream
+
+            <div className='list_wrapper'>
+                {getSearchResult().map((it) => (
+                    <TodoItem
+                        key={it.id}
+                        {...it}
+                        onUpdate={onUpdate}
+                        onDelete={onDelete}
+                    />
+                ))}
+            </div>
+=======
+>>>>>>> Stashed changes
         </div>
-    </div>
     );
 };
 
